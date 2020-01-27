a62de5d1ce83bde8b55215e407bd28076c58a46d
o2_sevcik <o2_sevcik@utb.cz>
2020-01-27 17:58:21 +0100

    Upload

---
+++ src/functions.c
@@ -0,0 +1,24 @@
+/**
+ * @file       functions.c
+ * @author     Horak
+ * @date       2019.6
+ * @brief      Implementation of function.h header file
+ * ******************************************
+ * @par       COPYRIGHT NOTICE (c) 2019 TBU in Zlin. All rights reserved.
+ */
+
+/*Private includes: ---------------------------------------------------------*/
+#include "functions.h"
+
+/*Class definition: ---------------------------------------------------------*/
+FILE* Functions_LoadStream(streamType type, int argc, char** argv) {
+  (void)type;
+  (void)argc;
+  (void)argv;
+  return NULL;
+}
+
+void Functions_RemoveNotes(FILE* input, FILE* output) {
+  (void)input;
+  (void)output;
+}
